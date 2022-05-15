90fe2749375ef0ba370d6efada8656eeb44a27c6
Isaiah-Sama <zeldaplayergl@gmail.com>
2022-05-15 16:36:29 -0400

    Added logic to database

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 """This file will contain the handling for the database"""
+import errors
 
 from typing import List, Tuple
 from sqlite3 import connect
@@ -13,7 +14,7 @@ class Database:
     Methods:
         setup(): Used to setup the database.
         query_all_projects(): Used to get all projects from the db.
-        query_one_project_by_title(title): Used to get a project by its title.
+        query_project_by_title(title): Used to get a project by its title.
         add_new_project(title, description, state, comp_date)
         delete_project_by_title(title): Used to delete a project by its title.
         delete_all_projects(): Used to delete all projects stored.
@@ -39,24 +40,35 @@ class Database:
         """Method used to retrieve all projects from the database.
         
         Returns:
-            list | None"""
+            list | None
+            
+        Raises: 
+            MissingEntryError"""
+
         with connect(self.DB_NAME) as db:
             cursor = db.execute("SELECT * FROM ProjectTable")
             rows = cursor.fetchall()
+
+        if not rows: raise errors.MissingEntryError("No entries have been made as yet.")
         return rows
 
-    def query_one_project_by_title(self, title:str) -> Tuple | None:
+    def query_project_by_title(self, title:str) -> Tuple | None:
         """Used to retrieve a specific project's information by it's title.
         
         Args:
             title (str): The title of the project. Case insensitive.
             
         Returns:
-            Tuple | None"""
+            Tuple | None
+            
+        Raises:
+            MissingEntryErrorr"""
         
         with connect(self.DB_NAME) as db:
             cursor = db.execute("SELECT * FROM ProjectTable WHERE title = ?", (title,))
             row = cursor.fetchone()
+
+        if not row: raise errors.MissingEntryError("No entry with the title ", title, " exists")
 
         return row 
 
@@ -70,9 +82,15 @@ class Database:
             comp_date (str): Expected Completion date of the project.
             
         Returns:
-            bool"""
+            bool
+            
+        Raises:
+            DuplicateEntryError"""
 
         info = (title, description, state, comp_date)
+
+        exists = self.query_project_by_title(title)
+        if exists: raise errors.DuplicateEntryError("Name: ", title)
 
         with connect(self.DB_NAME) as db:
             db.execute("INSERT INTO ProjectTable (title, description, state, comp_date, date_added) VALUES (?, ?, ?, ?, datetime('now', 'localtime'))", info)
@@ -84,7 +102,16 @@ class Database:
         """Used to delete a project by it's title
         
         Args:
-            title (str): The title of the project to delete."""
+            title (str): The title of the project to delete.
+            
+        Raises:
+            MissingEntryError"""
+
+
+        try:
+            self.query_project_by_title(title)
+        except errors.MissingEntryError:
+            raise errors.MissingEntryError("Cannot delete nonexistent entry with title ", title)
 
         with connect(self.DB_NAME) as db:
             db.execute("DELETE FROM ProjectTable WHERE title=?", (title, ))
@@ -93,7 +120,12 @@ class Database:
         return
 
     def delete_all_projects(self):
-        """Used to delete all projects in the database."""
+        """Used to delete all projects in the database.
+        
+        Raises:
+            If no entry exists, will raise MissingEntryError"""
+
+        self.query_all_projects()
 
         with connect(self.DB_NAME) as db:
             db.execute("DELETE FROM ProjectTable")
@@ -107,9 +139,24 @@ class Database:
             new_title(str): The new title of the project.
             description(str): The new description of the project.
             state (str): The new state of the project.
-            comp_date (str): The new completion date of the project."""
+            comp_date (str): The new completion date of the project.
+            
+        Raises:
+            If project does not exist, will raise MissingEntryError."""
 
-        data = ()
+        # May raise MissingEntryError
+        og = self.query_project_by_title(title)
+
+        data = list(og)
+        
+        if new_title:
+            data[0] = new_title
+        if description:
+            data[1] = description
+        if state:
+            data[2] = state
+        if comp_date:
+            data[4] = comp_date
 
         with connect(self.DB_NAME) as db:
             db.execute("""UPDATE ProjectTable 
@@ -117,7 +164,7 @@ class Database:
             description = ?,
             state = ?,
             comp_date = ?
-            WHERE title = ?""", data)
+            WHERE title = ?""", (*data, title))
             db.commit()
 
 
