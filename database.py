684f6185ae9162b98fc805e84421815e4f3fa551
Isaiah-Sama <zeldaplayergl@gmail.com>
2022-05-15 16:01:26 -0400

    Added base setup for db

--- database.py
+++ database.py
@@ -1,3 +1,20 @@
 """This file will contain the handling for the database"""
 
 from sqlite3 import connect
+
+
+class Database:
+    """Class designed to handle connections and calls to the database.
+    
+    Attrs:
+        DB_NAME (str): The name of the database.
+        
+    Methods:
+        setup(): Used to setup the database."""
+
+    DB_NAME = "projects.sqlite3"
+
+    def __init__(self):
+        self.setup()
+
+
