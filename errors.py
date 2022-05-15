ccf55ba17680e584509adc8806012e7bdadf565f
Isaiah-Sama <zeldaplayergl@gmail.com>
2022-05-15 17:45:13 -0400

    Added a base error

--- errors.py
+++ errors.py
@@ -1,13 +1,21 @@
 """File which will contain my custom errors for the sole purpose of the fact that I can :)"""
 
-class MissingEntryError(Exception):
+
+class MyErrors(Exception):
+    """Base class for me Exceptions."""
+
+    def __init__(self, *args):
+        super().__init__(args)
+
+
+class MissingEntryError(MyErrors):
     """Exception raised when a queried database entry does not exist."""
 
     def __init__(self, *args):
         base_message = "MissingEntryError: A value that you queried does not exist in the database."
         super().__init__(base_message, args)
 
-class DuplicateEntryError(Exception):
+class DuplicateEntryError(MyErrors):
     """Exception raised when an entry with a given title already exists."""
 
     def __init__(self, *args):
