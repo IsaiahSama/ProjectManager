bd41d78ff8cf78f43d82cb33b7fce00c795f2996
Isaiah-Sama <zeldaplayergl@gmail.com>
2022-05-15 16:41:38 -0400

    Added base for mechanics

---
+++ mechanics.py
@@ -0,0 +1,23 @@
+"""Will contain all of the mechanics used by the program."""
+
+class Validator:
+    """Class used to validate input
+    
+    Attrs:
+        None
+        
+    Methods:
+        validate_title(title): Used to validate a given title.
+        validate_yes_no(prompt:str): Used to validate a yes, no response."""
+
+class Menu:
+    """The main menu of the program.
+    
+    Attrs:
+        None
+        
+    Methods:
+        None"""
+
+    def __init__(self) -> None:
+        pass
